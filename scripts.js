67b126a8763004855b9af673b43d81ea58c67ede
Michael Kilonzo <68613943+michaelkilonzo@users.noreply.github.com>
2024-12-17 08:36:16 -0500

    add button and slider functionality

--- scripts.js
+++ scripts.js
@@ -1,27 +1,86 @@
+// Constants
 const GRIDSIDE = 600;
-let rows = 16;
+
+// Variables
+let rows = 5;
 let cols = rows;
 
+// DOM Elements
 const gridContainerElement = document.querySelector('.grid-container');
-gridContainerElement.style.width = `${GRIDSIDE}px`;
-gridContainerElement.style.height = `${GRIDSIDE}px`;
+const toggleGridBtn = document.querySelector('#btn-grid');
+const clearGridBtn = document.querySelector('#btn-clear');
+const toggleGrayscaleBtn = document.querySelector('#btn-grayscale');
+const toggleRainbowBtn = document.querySelector('#btn-rainbow');
+const toggleEraserBtn = document.querySelector('#btn-eraser');
+const slider = document.querySelector(".slider");
 
-function createGrid(){
-    for (let i = 0; i < rows * cols; i++){
+// Flags
+let drawFlags = new Map();
+drawFlags.set('isGrayscale', false);
+drawFlags.set('isRainbow', false);
+drawFlags.set('isEraser', false);
+
+// Functions
+function createGrid() {
+    gridContainerElement.innerHTML = '';
+    gridContainerElement.style.width = `${GRIDSIDE}px`;
+    gridContainerElement.style.height = `${GRIDSIDE}px`;
+
+    for (let i = 0; i < (2 ** rows) * (2 ** cols); i++) {
         const gridCell = document.createElement('div');
         gridCell.classList.add('cell');
-        gridCell.style.width = `${(GRIDSIDE / cols) - 2}px`;
-        gridCell.style.height = `${(GRIDSIDE / rows) - 2}px`;
+        gridCell.style.width = `${(GRIDSIDE / 2 ** cols) - 2}px`;
+        gridCell.style.height = `${(GRIDSIDE / 2 ** rows) - 2}px`;
 
         gridContainerElement.appendChild(gridCell);
-
-        gridCell.addEventListener('mouseover', function(){
-            this.style.backgroundColor = 'black';
-        });
+        gridCell.addEventListener('mouseover', draw);
     }
 }
 
-function getRandomColor(){
+function draw() {
+    if (drawFlags.get('isGrayscale')) {
+        this.style.backgroundColor = getGrayscaleColor();
+    }
+    else if (drawFlags.get('isRainbow')) {
+        this.style.backgroundColor = getRandomColor();
+    }
+    else if (drawFlags.get('isEraser')) {
+        this.style.backgroundColor = 'white';
+    }
+    else {
+        this.style.backgroundColor = 'black';
+    }
+}
+
+function toggleGrid() {
+    const defaultBorder = '1px solid rgb(245, 245, 245)';
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        if (window.getComputedStyle(cell).border === defaultBorder) {
+            cell.style.border = 'none';
+        } else {
+            cell.style.border = defaultBorder;
+        }
+    });
+}
+
+function clearGrid() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.style.backgroundColor = 'white');
+}
+
+function toggleMode(mode) {
+    drawFlags.forEach((_, key) => drawFlags.set(key, key === mode ? !drawFlags.get(key) : false));
+}
+
+function getGrayscaleColor() {
+    const value = Math.floor(Math.random() * 256);
+    const hexValue = value.toString(16).padStart(2, '0').toUpperCase();
+    const color = `#${hexValue}${hexValue}${hexValue}`;
+    return color;
+}
+
+function getRandomColor() {
     const letters = '0123456789ABCDEF';
     let color = '#';
     for (let i = 0; i < 6; i++) {
@@ -30,5 +89,18 @@ function getRandomColor(){
     return color;
 }
 
+function gridSlider() {
+    rows = cols = this.value;
+    createGrid();
+}
 
+// Event Listeners
+toggleGridBtn.addEventListener('click', toggleGrid);
+clearGridBtn.addEventListener('click', clearGrid);
+toggleGrayscaleBtn.addEventListener('click', () => toggleMode('isGrayscale'));
+toggleRainbowBtn.addEventListener('click', () => toggleMode('isRainbow'));
+toggleEraserBtn.addEventListener('click', () => toggleMode('isEraser'));
+slider.addEventListener('input', gridSlider);
+
+// Initial Grid Creation
 createGrid();
